287fc8c26bbc1a2e2e2e0b9c1c21ec0dc2a2db48
Emily <tsengemily818@gmail.com>
2020-10-12 00:44:39 +0800

    project init

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
+
+const app = express();
+const port = 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { restaurants: restaurantList.results });
+});
+app.get("/restaurants/:restaurants_id", (req, res) => {
+  const restaurant = restaurantList.results.find(
+    (restaurant) => restaurant.id.toString() === req.params.restaurants_id
+  );
+  res.render("show", { restaurant: restaurant });
+});
+app.get("/search", (req, res) => {
+  const keyword = req.query.keyword;
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return (
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    );
+  });
+  res.render("index", { restaurants: restaurants, keyword: keyword });
+});
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
