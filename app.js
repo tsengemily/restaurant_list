fdafa3d2af54ae654855a40e2f70fdeaa536b166
Emily <tsengemily818@gmail.com>
2020-11-13 00:15:30 +0800

    Renew index page by restaurant seeder

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const restaurantList = require("./restaurant.json");
 const mongoose = require("mongoose");
 const Restaurant = require("./models/restaurant");
 const app = express();
@@ -28,24 +27,28 @@ db.once("open", () => {
 
 //route setting
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((error) => console.log(error));
 });
-app.get("/restaurants/:restaurants_id", (req, res) => {
-  const restaurant = restaurantList.results.find(
-    (restaurant) => restaurant.id.toString() === req.params.restaurants_id
-  );
-  res.render("show", { restaurant: restaurant });
-});
-app.get("/search", (req, res) => {
-  const keyword = req.query.keyword;
-  const restaurants = restaurantList.results.filter((restaurant) => {
-    return (
-      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-    );
-  });
-  res.render("index", { restaurants: restaurants, keyword: keyword });
-});
+
+//app.get("/restaurants/:restaurants_id", (req, res) => {
+// const restaurant = restaurantList.results.find(
+// (restaurant) => restaurant.id.toString() === req.params.restaurants_id
+//);
+// res.render("show", { restaurant: restaurant });
+//});
+//app.get("/search", (req, res) => {
+//const keyword = req.query.keyword;
+//const restaurants = restaurantList.results.filter((restaurant) => {
+//return (
+//  restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+//   restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+// );
+// });
+// res.render("index", { restaurants: restaurants, keyword: keyword });
+//});
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -15,15 +15,15 @@ db.once("open", () => {
   console.log("mongodb connected !");
   seedRestaurant.forEach((r) => {
     Restaurant.create({
-      name: "r.name",
-      name_en: "r.name_en",
-      category: "r.category",
-      image: "r.image",
-      location: "r.location",
-      phone: "r.phone",
-      google_map: "r.google_map",
+      name: r.name,
+      name_en: r.name_en,
+      category: r.category,
+      image: r.image,
+      location: r.location,
+      phone: r.phone,
+      google_map: r.google_map,
       rating: r.rating,
-      description: "r.description",
+      description: r.description,
     });
   });
   console.log("done");
