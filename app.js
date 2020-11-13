94de9333ee65603f37a016422347b5b1e8ba8b03
Emily <tsengemily818@gmail.com>
2020-11-13 15:26:46 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -33,12 +33,48 @@ app.get("/", (req, res) => {
     .then((restaurants) => res.render("index", { restaurants }))
     .catch((error) => console.log(error));
 });
-//瀏覽一筆
+//detail
 app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id;
   return Restaurant.findById(id)
     .lean()
     .then((restaurant) => res.render("show", { restaurant }))
+    .catch((error) => console.log(error));
+});
+//get edit page
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error));
+});
+//edit
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const nameEn = req.body.name_en;
+  const category = req.body.category;
+  const image = req.body.image;
+  const location = req.body.location;
+  const phone = req.body.phone;
+  const googleMap = req.body.google_map;
+  const rating = req.body.rating;
+  const description = req.body.description;
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name;
+      restaurant.name_en = nameEn;
+      restaurant.category = category;
+      restaurant.image = image;
+      restaurant.location = location;
+      restaurant.phone = phone;
+      restaurant.google_map = googleMap;
+      restaurant.rating = rating;
+      restaurant.description = description;
+      return restaurant.save();
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
     .catch((error) => console.log(error));
 });
 
