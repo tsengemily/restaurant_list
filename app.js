fffd9584c7a21484e75565369db381fa067449f3
Emily <tsengemily818@gmail.com>
2020-11-17 20:19:34 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const Restaurant = require('./models/restaurant')
+const routes = require('./routes')
+
 const app = express()
 const port = 3000
 
@@ -13,6 +14,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)
 
 //連線資料庫
 mongoose.connect('mongodb://localhost/restaurant-list', {
@@ -25,111 +27,6 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected !')
-})
-
-//route setting
-//index
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then((restaurants) => res.render('index', { restaurants }))
-    .catch((error) => console.log(error))
-})
-//get create page
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-//create
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const nameEn = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const googleMap = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({
-    name,
-    nameEn,
-    category,
-    image,
-    location,
-    phone,
-    googleMap,
-    rating,
-    description,
-  })
-    .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
-})
-//detail
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .catch((error) => console.log(error))
-})
-//get edit page
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch((error) => console.log(error))
-})
-//edit
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const nameEn = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const googleMap = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then((restaurant) => {
-      restaurant.name = name
-      restaurant.name_en = nameEn
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = googleMap
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch((error) => console.log(error))
-})
-//delete
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .then((restaurant) => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch((error) => console.log(error))
-})
-//search
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  Restaurant.find()
-    .lean()
-    .then((restaurants) => {
-      return restaurants.filter(
-        (restaurant) =>
-          restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
-          restaurant.category.toLowerCase().includes(keyword.toLowerCase())
-      )
-    })
-    .then((restaurants) => res.render('index', { restaurants, keyword }))
-    .catch((error) => console.log(error))
 })
 
 app.listen(port, () => {
---
+++ routes/modules/restaurants.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/restaurant')
+
+//search
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      return restaurants.filter(
+        (restaurant) =>
+          restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+          restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+      )
+    })
+    .then((restaurants) => res.render('index', { restaurants, keyword }))
+    .catch((error) => console.log(error))
+})
+//get create page
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+//create
+router.post('/', (req, res) => {
+  const {
+    name,
+    nameEn,
+    category,
+    image,
+    location,
+    phone,
+    googleMap,
+    rating,
+    description,
+  } = req.body
+  return Restaurant.create({
+    name,
+    nameEn,
+    category,
+    image,
+    location,
+    phone,
+    googleMap,
+    rating,
+    description,
+  })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+//detail
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch((error) => console.log(error))
+})
+//get edit page
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
+})
+//edit
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const {
+    name,
+    nameEn,
+    category,
+    image,
+    location,
+    phone,
+    googleMap,
+    rating,
+    description,
+  } = req.body
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name
+      restaurant.name_en = nameEn
+      restaurant.category = category
+      restaurant.image = image
+      restaurant.location = location
+      restaurant.phone = phone
+      restaurant.google_map = googleMap
+      restaurant.rating = rating
+      restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error))
+})
+//delete
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
+module.exports = router
