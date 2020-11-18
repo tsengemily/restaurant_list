5186ac6246840fa47e205c75440ed78ca409c602
Emily <tsengemily818@gmail.com>
2020-11-18 15:10:25 +0800

    feat: add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,10 +3,32 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then((restaurants) => res.render('index', { restaurants }))
-    .catch((error) => console.log(error))
+  const sorting = req.query.sorting
+  switch (sorting) {
+    case '_id-asc':
+      return Restaurant.find()
+        .lean()
+        .sort({ _id: 'asc' })
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch((error) => console.log(error))
+    case '_id-desc':
+      return Restaurant.find()
+        .lean()
+        .sort({ _id: 'desc' })
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch((error) => console.log(error))
+    case 'category':
+      return Restaurant.find()
+        .lean()
+        .sort({ category: 'asc' })
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch((error) => console.log(error))
+    default:
+      return Restaurant.find()
+        .lean()
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch((error) => console.log(error))
+  }
 })
 
 module.exports = router
